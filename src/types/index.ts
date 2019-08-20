a074e23a64a6fbf9836fa6d6f857ad841bbdf7e5
kkstrive <kkstrive0124@gmail.com>
2019-08-21 02:57:41 +0800

    feat(examples): 增加例子server服务

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Methods =
+  | 'get'
+  | 'GET'
+  | 'delele'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Methods
+  data?: any
+  params: any
+}
